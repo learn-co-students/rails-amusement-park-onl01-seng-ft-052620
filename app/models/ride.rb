040884deedbfdfb8c0af32a096f90ae2e2bdf50b
charlie763 <cwisoff@gmail.com>
2020-08-11 08:05:47 -0600

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,25 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if user.tickets < attraction.tickets
+      errors.messages[:tickets] = "You do not have enough tickets to ride the #{attraction.name}."
+    end
+
+    if user.height < attraction.min_height
+      errors.messages[:height] = "You are not tall enough to ride the #{attraction.name}."
+    end
+
+    if errors.messages.empty?
+      user.update(
+        tickets: user.tickets - attraction.tickets,
+        nausea: user.nausea + attraction.nausea_rating,
+        happiness: user.happiness + attraction.happiness_rating,
+      )
+      "Thanks for riding the #{attraction.name}!"
+    else
+      "Sorry. " + errors.messages.values.join(" ")
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  validates :password, presence: true, on: [:create, :save]
+
+  def mood
+    nausea > happiness ? "sad" : "happy" if nausea && happiness
+  end
 end
